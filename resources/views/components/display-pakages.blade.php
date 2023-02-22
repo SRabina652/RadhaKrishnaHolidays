114ed2065850d754bb3bc2ab931b3218ae6c5f12
SRabina652 <srabina19@tbc.edu.np>
2023-02-22 13:25:40 +0545

    Admin dashboard layout was made along with mrc of the pakageController

---
+++ resources/views/components/display-pakages.blade.php
@@ -0,0 +1,41 @@
+<div class="mt-5">
+<table class="table">
+ <thead class="bg-success text-white fw-bold">
+    <th>Pakage Name</th>
+    <th>pakage Price</th>
+    <th>Pakage Image</th>
+    <th>Description</th>
+    <th>View</th>
+    <th>Edit</th>
+    <th>Delete</th>
+ </thead>
+ <tbody>
+ @foreach ($products as $product)
+    <tr>
+    <td class="align-middle">{{$product->ProductName}}</td>
+    <td class="align-middle">{{$product->ProductPrice}}</td>
+    <td class="align-middle">{{$product->category?->name}}</td>
+    <td class="align-middle">{{$product->Quantity}}</td>
+    <td class="align-middle"><img src="{{ asset('uploads/' .$product->ProductImage) }}" class="img-thumbnail"></td>
+    <td><a href="{{route('product.edit',$product->id)}}" class="btn btn-light">Edit</a></td>    
+    <form action="{{route('product.destroy',$product->id)}}" method="POST">
+        @csrf
+        @method('DELETE')
+    <!-- <td><a href="" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete')">Delete</a></td>     -->
+    <td>
+    <button type="submit" class="btn btn-light" onclick="return confirm('Are you sure you want to delete')"> Delete
+</button>
+    </td>
+
+    </form>
+</tr>
+ @endforeach
+ </tbody>
+</table>
+<div class="d-flex">
+    <div class="mx-auto">
+        {!! $products->links() !!}
+    </div>
+
+</div>
+</div>
